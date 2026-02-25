953f4770fd937bf08e62b7c81d780df385d4dee7
ferzik85 <sersid@bk.ru>
2026-02-25 12:24:20 +0100

    updated workflow and added export to out dir on build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     qualities: [25, 50, 75, 100],
+    unoptimized: true
   },
 };
 
