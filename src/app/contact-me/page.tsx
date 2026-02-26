24fd89d48052c2f829ce168599c3a02b1755a70b
ferzik85 <sersid@bk.ru>
2026-02-26 14:03:57 +0100

    test

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -2,5 +2,6 @@ import ContactForm from "./contactForm";
 import { WEB3FORMS_ACCESS_KEY } from "../../utils/constants";
 
 export default function ContactPage() {
+	console.log("WEB3FORMS_ACCESS_KEY (server):", WEB3FORMS_ACCESS_KEY);
   return <ContactForm accessKey={WEB3FORMS_ACCESS_KEY} />;  
 }
