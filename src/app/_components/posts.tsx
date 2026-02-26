1b6ab4307db691d9e3510c695067bf86af7485fa
ferzik85 <sersid@bk.ru>
2026-02-26 12:12:50 +0100

    added welcome

--- src/app/_components/posts.tsx
+++ src/app/_components/posts.tsx
@@ -1,11 +1,22 @@
 import { postsMetadata } from "../posts-metadata";
 import PostCard from "./postCard";
-import styles from './styles/posts.module.css'
+import styles from "./styles/posts.module.css";
 
 export default async function Posts() {
   const listItems = postsMetadata.map((post) => (
     <PostCard key={post.id} {...post} />
   ));
 
-  return <div className={styles.container}>{listItems}</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.welcome}>
+        <div>
+          WELCOME, LUBIMKA<span>❤️</span>
+        </div>
+        <div>HAPPY INTERNATIONAL WOMEN’S DAY!</div>
+        <div>THIS SITE IS MY GIFT FOR YOU</div>
+      </div>
+      {listItems}
+    </div>
+  );
 }
--- src/app/posts-metadata.ts
+++ src/app/posts-metadata.ts
@@ -10,7 +10,7 @@ export const postsMetadata: Post[] = [
     imagePath: "/assets/post1/image1.webp",
     imageWidth: 1441,
     imageHeight: 727,
-    preview: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+    preview: "Here you can write your first public blog. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
   },
   {
     id: 1,
@@ -21,6 +21,6 @@ export const postsMetadata: Post[] = [
     imagePath: "/assets/post2/image2.webp",
     imageWidth: 3000,
     imageHeight: 1159,
-    preview: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
+    preview: "Here you can write your second public blog. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
   },
 ];
